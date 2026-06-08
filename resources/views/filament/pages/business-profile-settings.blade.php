97827a9fd1092db4a8a5d5281771ff87ab7e5473
Ridho Robbi Pasi <ridhorobbipasi@gmail.com>
2026-06-08 22:59:56 +0700

    fix: change form to wire:submit and standard form wrapper to ensure Livewire captures the submit event

--- resources/views/filament/pages/business-profile-settings.blade.php
+++ resources/views/filament/pages/business-profile-settings.blade.php
@@ -1,8 +1,8 @@
 <x-filament-panels::page>
-    <form wire:submit="save">
+    <form wire:submit="save" class="space-y-6">
         {{ $this->form }}
 
-        <div class="flex items-center justify-end gap-x-3 mt-4">
+        <div class="flex flex-wrap items-center gap-4 justify-start">
             <x-filament::button type="submit" size="lg" color="primary" wire:target="save">
                 Simpan Perubahan
             </x-filament::button>
