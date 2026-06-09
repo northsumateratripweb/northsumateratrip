98cb521b0d6c509a04b5779db60e950a6cf13849
Ridho Robbi Pasi <ridhorobbipasi@gmail.com>
2026-06-09 21:39:39 +0700

    Fix ERR_TOO_MANY_REDIRECTS by trusting proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,6 +26,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
 
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+
         $middleware->use([
             SecurityHeaders::class,
             PerformanceMonitoring::class,
